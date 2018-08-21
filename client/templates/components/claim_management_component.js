2e5c0d145bf6b4c57875fee4a70c449e0746b2cc
nghuubaotrung <bin1302@gmail.com>
2018-08-21 21:16:59 +0900

    added policy controller smart contract

--- client/templates/components/claim_management_component.js
+++ client/templates/components/claim_management_component.js
@@ -11,164 +11,20 @@ Template.claimManagementComponent.helpers({
         "hospital": "AAA 病院",
         "insurer": "AAA 生命保険会社",
         "address": "0xa12e5a79d1746501d4c6a2a92c1e49bcb5a487ec",
-        "abi": [
-        	{
-        		"constant": false,
-        		"inputs": [
-        			{
-        				"name": "hellowAddress",
-        				"type": "address"
-        			}
-        		],
-        		"name": "registerBonjour",
-        		"outputs": [],
-        		"payable": true,
-        		"stateMutability": "payable",
-        		"type": "function"
-        	},
-        	{
-        		"constant": false,
-        		"inputs": [],
-        		"name": "notifyCall",
-        		"outputs": [],
-        		"payable": true,
-        		"stateMutability": "payable",
-        		"type": "function"
-        	},
-        	{
-        		"constant": true,
-        		"inputs": [],
-        		"name": "s",
-        		"outputs": [
-        			{
-        				"name": "",
-        				"type": "string"
-        			}
-        		],
-        		"payable": false,
-        		"stateMutability": "view",
-        		"type": "function"
-        	},
-        	{
-        		"constant": false,
-        		"inputs": [],
-        		"name": "clearString",
-        		"outputs": [],
-        		"payable": true,
-        		"stateMutability": "payable",
-        		"type": "function"
-        	}
-        ]
       },
       {
         "name": "Policy 2",
         "policy_holder": "BBB 様",
         "hospital": "AAA 病院",
         "insurer": "AAA 生命保険会社",
-        "address": "0x76ad8408304d3447f1b54ae3a945b519fd847596",
-        "abi": [
-        	{
-        		"constant": false,
-        		"inputs": [
-        			{
-        				"name": "hellowAddress",
-        				"type": "address"
-        			}
-        		],
-        		"name": "registerBonjour",
-        		"outputs": [],
-        		"payable": true,
-        		"stateMutability": "payable",
-        		"type": "function"
-        	},
-        	{
-        		"constant": false,
-        		"inputs": [],
-        		"name": "notifyCall",
-        		"outputs": [],
-        		"payable": true,
-        		"stateMutability": "payable",
-        		"type": "function"
-        	},
-        	{
-        		"constant": true,
-        		"inputs": [],
-        		"name": "s",
-        		"outputs": [
-        			{
-        				"name": "",
-        				"type": "string"
-        			}
-        		],
-        		"payable": false,
-        		"stateMutability": "view",
-        		"type": "function"
-        	},
-        	{
-        		"constant": false,
-        		"inputs": [],
-        		"name": "clearString",
-        		"outputs": [],
-        		"payable": true,
-        		"stateMutability": "payable",
-        		"type": "function"
-        	}
-        ]
+        "address": "0x76ad8408304d3447f1b54ae3a945b519fd847596"
       },
       {
         "name": "Policy 3",
         "policy_holder": "BBB 様",
         "hospital": "AAA 病院",
         "insurer": "AAA 生命保険会社",
-        "address": "0x076f8d5eefbfd95cb452831007575534252265bf",
-        "abi": [
-        	{
-        		"constant": false,
-        		"inputs": [
-        			{
-        				"name": "hellowAddress",
-        				"type": "address"
-        			}
-        		],
-        		"name": "registerBonjour",
-        		"outputs": [],
-        		"payable": true,
-        		"stateMutability": "payable",
-        		"type": "function"
-        	},
-        	{
-        		"constant": false,
-        		"inputs": [],
-        		"name": "notifyCall",
-        		"outputs": [],
-        		"payable": true,
-        		"stateMutability": "payable",
-        		"type": "function"
-        	},
-        	{
-        		"constant": true,
-        		"inputs": [],
-        		"name": "s",
-        		"outputs": [
-        			{
-        				"name": "",
-        				"type": "string"
-        			}
-        		],
-        		"payable": false,
-        		"stateMutability": "view",
-        		"type": "function"
-        	},
-        	{
-        		"constant": false,
-        		"inputs": [],
-        		"name": "clearString",
-        		"outputs": [],
-        		"payable": true,
-        		"stateMutability": "payable",
-        		"type": "function"
-        	}
-        ]
+        "address": "0x076f8d5eefbfd95cb452831007575534252265bf"
       }
     ]
   }
@@ -197,23 +53,130 @@ Template.claimItem.events({
   'click': function(){
     console.log("Policy ID:" + this.address);
 
-    var contractAddress = this.address;
-    var abi = this.abi;
+    var policyContractAddress = this.address;
+    // var policyABI = this.abi;
 
+    var policyControllerAddress = "0xd4fe9170ff3241096f6cf1679c31159586cd3ac4";
+    var policyControllerABI = [
+      {
+    		"constant": false,
+    		"inputs": [
+    			{
+    				"name": "bonjourAddress",
+    				"type": "address"
+    			}
+    		],
+    		"name": "callBonjuor",
+    		"outputs": [],
+    		"payable": false,
+    		"stateMutability": "nonpayable",
+    		"type": "function"
+    	},
+    	{
+    		"constant": false,
+    		"inputs": [],
+    		"name": "callBonjuors",
+    		"outputs": [],
+    		"payable": true,
+    		"stateMutability": "payable",
+    		"type": "function"
+    	},
+    	{
+    		"constant": true,
+    		"inputs": [
+    			{
+    				"name": "",
+    				"type": "uint256"
+    			}
+    		],
+    		"name": "adList",
+    		"outputs": [
+    			{
+    				"name": "",
+    				"type": "address"
+    			}
+    		],
+    		"payable": false,
+    		"stateMutability": "view",
+    		"type": "function"
+    	},
+    	{
+    		"constant": false,
+    		"inputs": [
+    			{
+    				"name": "contractAddress",
+    				"type": "address"
+    			}
+    		],
+    		"name": "addContract",
+    		"outputs": [],
+    		"payable": true,
+    		"stateMutability": "payable",
+    		"type": "function"
+    	},
+    	{
+    		"constant": true,
+    		"inputs": [
+    			{
+    				"name": "",
+    				"type": "uint256"
+    			}
+    		],
+    		"name": "tmpAdList",
+    		"outputs": [
+    			{
+    				"name": "",
+    				"type": "address"
+    			}
+    		],
+    		"payable": false,
+    		"stateMutability": "view",
+    		"type": "function"
+    	},
+    	{
+    		"constant": true,
+    		"inputs": [],
+    		"name": "listLength",
+    		"outputs": [
+    			{
+    				"name": "",
+    				"type": "uint256"
+    			}
+    		],
+    		"payable": false,
+    		"stateMutability": "view",
+    		"type": "function"
+    	},
+    	{
+    		"inputs": [],
+    		"payable": false,
+    		"stateMutability": "nonpayable",
+    		"type": "constructor"
+    	}
+    ];
+
+    //実行ユーザの設定
     web3.eth.defaultAccount = web3.eth.accounts[0]
-    // eth.accounts[0]を毎回unlockしないといけない？？
+    // ユーザのunlock
+    web3.personal.unlockAccount(web3.eth.accounts[0], "123123123", 150000)
 
-    myContract = web3.eth.contract(abi).at(contractAddress);
-
-    myContract.notifyCall.sendTransaction({
-            from:web3.eth.accounts[0],
-            gas:4000000},function (error, result){ // gas priceをあげたの方実行が早い
-                if(!error){
-                    // Transaction IDを表示する
-                    console.log(result);
-                } else{
-                    console.log(error);
-                }
+    policyController = web3.eth.contract(policyControllerABI).at(policyControllerAddress);
+    policyController.callBonjuor.sendTransaction(policyContractAddress, {
+        from:web3.eth.accounts[0],
+        gas:4000000},function (error, result){ // gas priceをあげたの方実行が早い
+          if(!error){
+            // Transaction IDを表示する
+            console.log("Transaction ID: " + result);
+          } else{
+            console.log(error);
+          }
         });
-  }
+  },
 });
+
+// Template.claimManagementComponent.events({
+//   //証券のステータスの確認
+//   'click #recalil ': function(){
+//     console.log("recall claim");
+//   },
+// )};
