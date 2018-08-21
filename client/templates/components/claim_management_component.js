6fe419940e285a021189c818c9f90596a2fae28a
nghuubaotrung <bin1302@gmail.com>
2018-08-21 21:55:00 +0900

    updated UI

--- client/templates/components/claim_management_component.js
+++ client/templates/components/claim_management_component.js
@@ -9,21 +9,21 @@ Template.claimManagementComponent.helpers({
         "name": "Policy 1",
         "policy_holder": "AAA 様",
         "hospital": "AAA 病院",
-        "insurer": "AAA 生命保険会社",
+        "insurer": "死亡保険その1",
         "address": "0xa12e5a79d1746501d4c6a2a92c1e49bcb5a487ec",
       },
       {
         "name": "Policy 2",
         "policy_holder": "BBB 様",
         "hospital": "AAA 病院",
-        "insurer": "AAA 生命保険会社",
+        "insurer": "死亡保険その1",
         "address": "0x76ad8408304d3447f1b54ae3a945b519fd847596"
       },
       {
         "name": "Policy 3",
         "policy_holder": "BBB 様",
         "hospital": "AAA 病院",
-        "insurer": "AAA 生命保険会社",
+        "insurer": "死亡保険その3",
         "address": "0x076f8d5eefbfd95cb452831007575534252265bf"
       }
     ]
@@ -173,10 +173,3 @@ Template.claimItem.events({
         });
   },
 });
-
-// Template.claimManagementComponent.events({
-//   //証券のステータスの確認
-//   'click #recalil ': function(){
-//     console.log("recall claim");
-//   },
-// )};
